17f6415804404866ff38f26b737be07192a6e0af
Jungyu Park <parkjungyu09@gmail.com>
2024-12-09 23:22:44 +0900

    react-router-dom : outlet, params

--- react-study/src/App.js
+++ react-study/src/App.js
@@ -1,16 +1,23 @@
 import logo from "./logo.svg";
 import "./App.css";
+import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import Videos from "./pages/Videos";
+import Root from "./pages/Root";
+import VideoDetail from "./pages/VideoDetail";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <>home</>,
-    errorElement: <>Not Found🤔</>,
-  },
-  {
-    path: "/detail",
-    element: <>detail</>,
+    element: <Root />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "/videos", element: <Videos /> },
+      { path: "/videos/:videoId", element: <VideoDetail />}
+    ],
   },
 ]);
 
