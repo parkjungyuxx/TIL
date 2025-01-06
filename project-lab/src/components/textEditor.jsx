e006b0eb93a48cc8d7902f96af4c5a2f03840a5f
Jungyu Park <parkjungyu09@gmail.com>
2025-01-06 23:40:04 +0900

    react quill

--- project-lab/src/components/textEditor.jsx
+++ project-lab/src/components/textEditor.jsx
@@ -5,5 +5,12 @@ import "react-quill/dist/quill.snow.css";
 export default function MyComponent() {
   const [value, setValue] = useState("");
 
-  return <ReactQuill theme="snow" value={value} onChange={setValue} />;
+  return (
+    <ReactQuill
+      theme="snow"
+      value={value}
+      onChange={setValue}
+      style={{ width: "1000px", height: "720px" }}
+    />
+  );
 }
