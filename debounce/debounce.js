13f301e869d9d89e315287d6bd962709e79ac8d3
Jungyu Park <parkjungyu09@gmail.com>
2025-04-12 18:34:45 +0900

    생성자 함수 및 프로토타입 정리

--- debounce/debounce.js
+++ debounce/debounce.js
@@ -3,7 +3,7 @@
  * 디바운스(Debounce)는 짧은 시간 간격으로 연속해서 발생하는 이벤트를 그룹화하여 마지막 이벤트만 처리하는 프로그래밍 기법.
  */
 
-// 디바운스 함수
+// 디바운스 기법 사용 
 
 window.addEventListener("DOMContentLoaded", () => {
   const editDiv = document.getElementById("edit-div");
