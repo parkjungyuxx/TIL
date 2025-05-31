cc565098f5d5cf8cb3b1a243e3169efd4b9128d1
Jungyu Park <parkjungyu09@gmail.com>
2025-05-31 16:56:00 +0900

    poc: main 컴포넌트

--- devcourse_project3_poc/src/routes/pages/Home.tsx
+++ devcourse_project3_poc/src/routes/pages/Home.tsx
@@ -1,7 +1,12 @@
-export default function Home () {
+import mainimg from "../../assets/images/mainimg1.png";
+export default function Main() {
   return (
-    <>
-      <h1>Home</h1>
-    </>
+    <div className="h-full w-screen flex justify-center items-center flex-col">
+      <div className="text-[72px] text-center">
+        Meet the <br />
+        Pictionary Pros
+      </div>
+      <img src={mainimg} alt="메인이미지" className="w-[1080px] " />
+    </div>
   );
 }
