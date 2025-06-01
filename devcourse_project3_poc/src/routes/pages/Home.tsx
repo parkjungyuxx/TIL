93ac6132284feea8f23093facd40c46607936bf1
Jungyu Park <parkjungyu09@gmail.com>
2025-06-01 21:49:30 +0900

    🎨 로그인 버튼 생성 및 메인 화면 이미지

--- devcourse_project3_poc/src/routes/pages/Home.tsx
+++ devcourse_project3_poc/src/routes/pages/Home.tsx
@@ -1,10 +1,15 @@
-import mainimg from "../../assets/images/mainimg1.png";
+import { ArrowRight } from "lucide-react";
+import mainimg from "../../assets/images/mainimg.png";
 export default function Main() {
   return (
     <div className="h-full w-screen flex justify-center items-center flex-col">
       <div className="text-[72px] text-center">
         Meet the <br />
         Pictionary Pros
+      </div>
+      <div className="flex border border-[#222222] rounded-[8px] py-2 px-4 mt-4 gap-2">
+        <div>JOIN</div>
+        <ArrowRight className="w-4" />
       </div>
       <img src={mainimg} alt="메인이미지" className="w-[1080px] " />
     </div>
