ddabd8e5ea81c2f5d0d31a1b3870ca461acc3a5b
Jungyu Park <parkjungyu09@gmail.com>
2025-06-01 23:38:39 +0900

    🎨Navbar Hover 색상 변경

--- devcourse_project3_poc/src/components/common/Navbar.tsx
+++ devcourse_project3_poc/src/components/common/Navbar.tsx
@@ -1,21 +1,37 @@
+import { NavLink } from "react-router";
 import draw from "../../assets/images/streamline_pen-draw-solid.svg";
 
 export default function Navbar() {
   return (
     <>
       <nav className="py-10">
-        <div className="flex justify-center h-[72px]">
+        <div className="flex justify-center h-[62px]">
           <div className="flex bg-[#222222] p-2 rounded-[8px]">
             <div className="bg-[#FA5D29] rounded-[8px] flex items-center justify-center w-[56px]">
               <img src={draw} alt="draw icon" />
             </div>
-            <div className="text-white flex gap-4 justify-center items-center ml-[50px] mr-[50px]">
-              <a href="">Home</a>
-              <a href="">Community</a>
-              <a href="">About us</a>
+            <div className="text-white flex gap-4 justify-center items-center px-[20px] whitespace-nowrap">
+              <NavLink
+                to="/"
+                className="bg-[#fdfaf226] h-full px-4 items-center flex justify-center rounded-[12px]"
+              >
+                Home
+              </NavLink>
+              <NavLink 
+                to="/"
+                className="px-4 flex items-center justify-center h-full rounded-[12px] hover:bg-[#fdfaf226] transition-colors"
+              >
+                Community
+              </NavLink>
+              <NavLink 
+                to="/"
+                className="px-4 flex items-center justify-center h-full rounded-[12px] hover:bg-[#fdfaf226] transition-colors"
+              >
+                About us
+              </NavLink>
             </div>
           </div>
-          <div className="flex border border-[#222222] bg-[#E6FF42] text-center items-center rounded-[8px] ml-[14px] px-[30px]">
+          <div className="flex border border-[#222222] bg-[#c7ff69] text-center items-center rounded-[8px] ml-[14px] px-[30px]">
             Launch Game
           </div>
         </div>
